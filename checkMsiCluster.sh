c491fdd0b4ee858f4e21a8e16923927bdefec173
Adam Sharif <adsharif@microsoft.com>
2021-03-18 21:51:52 +0000

    Finalised v1, updated to use printf for output

--- checkMsiCluster.sh
+++ checkMsiCluster.sh
@@ -1,11 +1,17 @@
 # Bash script to check that the kubelet identity of the AKS cluster is assigned to all node pools (prevent image pull failures from ACR, etc.)
+# 
+# Usage: 
+# export clusterName=<clusterName>
+# export resourceGroup=<resourceGroup>
+# bash ./checkMsiCluster.sh
 
 RED='\033[0;31m'          # Red
 GREEN='\033[0;32m'        # Green
+YELLOW='\033[1;33m'
 NC='\033[0m'              # No Color
 
-clusterName=<clusterName>
-resourceGroup=<clusterResourceGroupName>
+# clusterName=<clusterName>
+# resourceGroup=<clusterResourceGroupName>
 # subscriptionId=<notUsed>
 
 kubeletIdentity=$(az aks show -n $clusterName -g $resourceGroup --query identityProfile.kubeletidentity.clientId -o tsv)
@@ -15,7 +21,7 @@ scalesets=$(az vmss list -o tsv --query "[].name" -g $nodeResourceGroup)
 
 # Iterate through VMSSes in the Infrastructure Resource Group of AKS cluster
 
-echo "** Checking AKS cluster $clusterName in Resource Group $resourceGroup for kubelet MSI assignment issues **"
+printf "${YELLOW}\n** Checking AKS cluster $clusterName in Resource Group $resourceGroup for kubelet MSI assignment issues **\n\n${NC}"
 
 while read -r scaleset
 do
@@ -25,15 +31,16 @@ do
 
     identityFound=0
 
+    
+    printf -- "---------------------------------------------------------------\n"
+    printf "vmss name: $scaleset\n"
+    printf "kubelet identity: $kubeletIdentity\n"
+
     # Iterate through the assigned identities on the current scaleset in the parent loop
 
     while read -r identity 
     do
-        echo "------------------------------------------------------"
-        echo "identity: $identity"
-        echo "kubelet identity: $kubeletIdentity"
-        echo "vmss name: $scaleset"
-        echo "------------------------------------------------------\n"
+        printf "identity: $identity\n"
 
         if [[ "$identity" == "$kubeletIdentity" ]]
         then 
@@ -42,11 +49,13 @@ do
 
     done <<<$identities
 
+    printf -- "---------------------------------------------------------------\n\n"
+
     if [ $identityFound -eq 1 ]
     then
-        echo -e "${GREEN}Found kubelet identity $kubeletIdentity on agent pool VMSS $scaleset\n${NC}"
+        printf "${GREEN}Found kubelet identity $kubeletIdentity on agent pool VMSS $scaleset\n\n${NC}"
     else
-        echo -e "${RED}Warning! Did not find kubelet identity $kubeletIdentity on agent pool VMSS $scaleset\n${NC}"
+        printf "${RED}Warning! Did not find kubelet identity $kubeletIdentity on agent pool VMSS $scaleset\n\n${NC}"
     fi
     
 done <<<$scalesets
